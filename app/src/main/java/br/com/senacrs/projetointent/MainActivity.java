17284736b84a20a52581fcedb3da74aea0ef55f6
Luis Henrique Ries <lhries@MacBook-Pro-de-Luis.local>
2018-06-09 07:39:55 -0300

    Projeto Intent

---
+++ app/src/main/java/br/com/senacrs/projetointent/MainActivity.java
@@ -0,0 +1,30 @@
+package br.com.senacrs.projetointent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirNavegador(View v){
+
+        EditText textoURL = (EditText) findViewById(R.id.textoURL);
+        String url = textoURL.getText().toString();
+
+        if (!url.startsWith("http://") && !url.startsWith("https://")){
+            url = "http://" + url;
+        }
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+}
